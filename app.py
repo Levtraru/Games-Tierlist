8734e464442fa72495ab023e71633ef68d292731
Levtraru <107771213+Levtraru@users.noreply.github.com>
2023-01-06 16:31:33 -0300

    change app.run

--- app.py
+++ app.py
@@ -249,4 +249,4 @@ def tier_F():
     return render_template('juegos/index.html', juegos=juegos, titulo=titulo)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
